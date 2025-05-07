78ec6a232e7643aebafc26fe94422c24d7c97bf4
Sasha Afanasienka <afanasienka@yandex.by>
2025-05-07 10:18:01 +0300

    fix: transitionend listener

--- src/test-example-assets/test-example-initialization.ts
+++ src/test-example-assets/test-example-initialization.ts
@@ -3,8 +3,14 @@ import { Accordions } from '../index';
 const component = document.querySelector('.component');
 // eslint-disable-next-line no-unused-vars
 const accordions = new Accordions({
-	parentElement: component,
-	isSingle: true,
+  parentElement: component,
+  isSingle: true,
+  devMode: true,
+  on: {
+    detailsTransitionEnd: () => {
+      console.log('transitionend');
+    },
+  },
 });
 
 // eslint-disable-next-line no-console
