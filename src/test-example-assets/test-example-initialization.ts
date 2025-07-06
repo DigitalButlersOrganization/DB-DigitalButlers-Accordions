33bd449874e3f7f33029f5ee9a1764b4feae0a92
Evgeniy Ivanchikay <ev.ivanchikov@gmail.com>
2025-07-06 23:01:04 +0300

    use new parameter isKeyboardTriggerAllowed

--- src/test-example-assets/test-example-initialization.ts
+++ src/test-example-assets/test-example-initialization.ts
@@ -3,14 +3,15 @@ import { Accordions } from '../index';
 const component = document.querySelector('.component');
 // eslint-disable-next-line no-unused-vars
 const accordions = new Accordions({
-  parentElement: component,
-  isSingle: true,
-  devMode: true,
-  on: {
-    detailsTransitionEnd: () => {
-      console.log('transitionend');
-    },
-  },
+	parentElement: component,
+	isSingle: true,
+	devMode: true,
+	isKeyboardTriggerAllowed: true,
+	on: {
+		detailsTransitionEnd: () => {
+			console.log('transitionend');
+		},
+	},
 });
 
 // eslint-disable-next-line no-console
